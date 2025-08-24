4eb4c516babd4eeb43e4487ef15c6c8ec00b09e3
Ravilesh <rravilesh@gmail.com>
2025-08-25 00:32:30 +0530

    change the database for  more secure, more reliable, and faster to develop by using Django's powerful built-in features.

--- Backend/coffee_backend/coffee_api/views.py
+++ Backend/coffee_backend/coffee_api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Products
+from .serializers import ProductSerializer
 
-# Create your views here.
+# This view handles GET (list all) and POST (create new) requests
+class ProductList(generics.ListCreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
